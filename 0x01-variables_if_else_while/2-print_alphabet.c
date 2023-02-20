e3fb22b3965d58a1f3899153460b7cafedaa41ee
Obiosaji <obiosajiqueen@gmail.com>
2023-02-19 23:36:00 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char sl;
+
+	for (sl = 'a'; sl <= 'z'; sl++)
+	{
+		putchar(sl);
+	}
+	putchar('\n');
+	return (0);
+}
