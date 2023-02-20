293d3bb25a33aea3a787445ac55a01eb29f0e4ce
Obiosaji <obiosajiqueen@gmail.com>
2023-02-20 00:11:08 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char al;
+
+	for (al = 'a'; al <= 'z'; al++)
+	{
+		if (al != 'e' && al != 'q')
+			putchar(al);
+	}
+	putchar('\n');
+	return (0);
+}
