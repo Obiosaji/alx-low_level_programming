2719d5bdfe6086707713030a87dafd53634850ea
Obiosaji <obiosajiqueen@gmail.com>
2023-02-22 13:55:40 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit
+ * @i: The number
+ * Return: value of the last digit
+ */
+int print_last_digit(int i)
+{
+	int ld = i % 10;
+
+	if (ld < 0)
+		ld *= -1;
+	_putchar(ld + '0');
+
+	return (0);
+}
