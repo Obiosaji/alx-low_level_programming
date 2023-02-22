964964ba670afc7731ad258df69868175bae6d31
Obiosaji <obiosajiqueen@gmail.com>
2023-02-21 19:28:10 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - print the alpha in lowercase
+ */
+void print_alphabet(void);
+{
+	char sl;
+
+	for (sl = 'a'; sl <= 'z'; sl++)
+		_putchar(sl);
+
+	_putchar('\n');
+}
