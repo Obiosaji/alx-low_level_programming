43d0cf9f0330140b2c498e04061042b81472645b
Obiosaji <obiosajiqueen@gmail.com>
2023-02-21 19:42:32 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alpha 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+	int sl;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (sl = 'a'; sl <= 'z'; sl++)
+			_putcha(sl);
+		_putchar('\n');
+	}
+}
