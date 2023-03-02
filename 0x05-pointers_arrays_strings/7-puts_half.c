acaf4471042a7aa2386090bd27bd0fb997212ee2
Obiosaji <obiosajiqueen@gmail.com>
2023-03-01 17:03:09 -0800

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts_half - a function that prints half of a string
+ * if odd len, n = (length_of_the_string - 1) / 2
+ * @str: input
+ * Return: half of input
+ */
+void puts_half(char *str)
+{
+	int c, n, longi;
+
+	longi = 0;
+
+	for (c = 0; str[c] != '\0'; c++)
+		longi++;
+	n = (longi / 2);
+
+	if ((longi % 2) == 1)
+		n = ((longi + 1) / 2);
+	for (c = n; str[c] != '\0'; c++)
+		_putchar(str[c]);
+	_putchar('\n');
+}
