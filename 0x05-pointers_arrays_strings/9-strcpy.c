a657f36c7a118676dde2f5b17d8e91e09fee1c2f
Obiosaji <obiosajiqueen@gmail.com>
2023-03-01 18:11:48 -0800

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * char*_strcpy - a function that copies the stringpointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int k = 0;
+	int l = 0;
+
+	while (*(src + k) != '\0')
+	{
+		k++;
+	}
+	for ( ; l < k ; l++)
+	{
+		dest[l] = src[l];
+	}
+	dest[k] = '\0';
+	return (dest);
+}
