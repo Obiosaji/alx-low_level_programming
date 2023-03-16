b9f11ff31066f57487829497a72ffe1dbc82774c
Obiosaji <obiosajiqueen@gmail.com>
2023-03-15 22:03:42 -0700

    1-args.c

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,7 +3,9 @@
                 
 int _putchar(char c);
 int _atoi(char *s);
-int main(int argc __attribute__((unused)), char *argv[]);                
+int main(int argc __attribute__((unused)), char *argv[]);
+int main(int argc, char *argv[]);
+int check_num(char *str);
                 
 #endif
 
